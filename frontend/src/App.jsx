1ac55c2576d5d8a2330de1d937cde26238e8e6f0
joyal m a <joyalaugustine12@gmail.com>
2026-01-07 22:03:31 +0530

    created home page route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/home'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7-vw] lg:px-[9vw]'>
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+
+      </Routes>
       
     </div>
   )
