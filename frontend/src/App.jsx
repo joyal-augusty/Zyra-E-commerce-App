56bc22efe49f0e7ea006144b373c0e40e0b55e33
joyal m a <joyalaugustine12@gmail.com>
2026-01-08 21:58:40 +0530

    added collection route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import Home from './pages/home'
+import Collection from './pages/Collection'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7-vw] lg:px-[9vw]'>
 
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} /> 
 
       </Routes>
       
