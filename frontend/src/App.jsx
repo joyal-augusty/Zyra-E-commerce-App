099476269ad208deba0e685b8f04fa25e5987dfd
joyal m a <joyalaugustine12@gmail.com>
2026-01-23 20:06:30 +0530

    created routes for all pages in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Routes ,Route} from 'react-router-dom'
 import Home from './pages/home'
 import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
 
 const App = () => {
   return (
@@ -10,7 +13,13 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} /> 
-
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders/> } />
       </Routes>
       
     </div>
