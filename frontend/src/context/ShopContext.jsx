0db73ee52e5cae8280326da28e099a9266e7c4d2
joyal m a <joyalaugustine12@gmail.com>
2026-02-07 23:51:36 +0530

    shopcontext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,18 @@
 import { createContext } from "react";
+import{products} from 
 
 export const ShopContext = createContext()
 
+const ShopContextProvider = (props) => {
+    const value = {
+
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
