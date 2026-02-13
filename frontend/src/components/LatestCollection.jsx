cc630809ead247108325bdd7a8ec70726a67c9cb
joyal m a <joyalaugustine12@gmail.com>
2026-02-13 23:12:26 +0530

    edited LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -4,14 +4,18 @@ import Title from './Title'
 
 const LatestCollection = () => {
 
-    const { products } = useContext(ShopContext)
+  const { products } = useContext(ShopContext)
+
     
   
   return (
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
           <Title text1={'LATEST'} text2={'COLLECTIONs'} />
-        </div>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Discover our latest collection of premium products designed to elevate your everyday style.
+        </p>
+      </div>
     </div>
   )
 }
