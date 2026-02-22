bd8d8815d3fe362756427607005a49598e98a148
joyal m a <joyalaugustine12@gmail.com>
2026-02-22 18:44:34 +0530

    added BestSeller products

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,10 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import ProductItem from './ProductItem';
+import Title from './Title';
 
 const BestSeller = () => {
 
   const { products } = useContext(ShopContext)
-  const [BestSeller, setBestSeller] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
     const bestProduct = products.filter((item) => (item.bestseller));
@@ -12,8 +14,22 @@ const BestSeller = () => {
   },[])
 
   return (
-    <div>
+    <div className='my-10'>
+      <div className='text-center text-3xl py-8'>
+        
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+        Effortless style meets all-day comfort in this ultra-soft, tailored fit designed to move with you from coffee dates to evening wind-downs.
+        </p>
+      </div>
       
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+          bestSeller.map((item, index) => (
+            <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+          ))
+      }
+      </div>
     </div>
   )
 }
--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useContext , useState , useEffect} from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
