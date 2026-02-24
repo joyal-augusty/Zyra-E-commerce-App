06e99384e0a26a5541a39211fb68a6d77d513726
joyal m a <joyalaugustine12@gmail.com>
2026-02-24 22:09:04 +0530

    added new file Newsletter.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
-      <BestSeller/>
+      <BestSeller />
+      <OurPolicy />
+      <Newsletter />
     </div>
   )
 }
