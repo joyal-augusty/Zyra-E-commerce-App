418c838e3bc0323475bfa724c82c98b4410dbd54
joyal m a <joyalaugustine12@gmail.com>
2026-03-03 19:43:32 +0530

    added sort options in Collections.jsx

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -56,6 +56,12 @@ const Collection = () => {
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'} />
+          {/*Product Sort */}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by:Relavent</option>
+            <option value="low-high">Sort by:Low to High</option>
+            <option value="high-low">Sort by:High to Low</option>
+          </select>
           </div>
       </div>
 
