b0da118eacb3e112b8645c017f06686b1e78663b
joyal m a <joyalaugustine12@gmail.com>
2026-03-07 08:12:49 +0530

    added products sort logic

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -8,23 +8,60 @@ const Collection = () => {
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
-  cosnt[subCategory, setSubCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
-      setCategory(prev=> prev.filter(item => item !== e.target.value))
+      setCategory(prev => prev.filter(item => item !== e.target.value))
     } else {
-      setCategory(prev => [...prev,e.target.value])
+      setCategory(prev => [...prev , e.target.value])
+    }
+  }
+
+  const toggleSubCategory = (e) => {
+    if (subcategory.includes(e.target.value)) {
+      setSubCategory(prev=> prev.filter(item => item !== e.target.value))
+    } else {
+      setSubCategory(prev => [...prev,e.target.value])
+    }
+  }
+
+  const applyFilter = () => {
+    let productCopy = products.slice();
+
+    if (category.length > 0) {
+      productCopy = productCopy.filter(item => category.includes(item.category));
+    }
+
+    if (subCategory > 0) {
+      productCopy = productCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+
+    setFilterProducts(productCopy)
+  }
+
+  const sortProduct = () => {
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a, b) => (a.price - b.price)));
+        break;
+      
+       case 'high-low':
+      setFilterProducts(fpCopy.sort((a, b) => (b.price - a.price)));
+        break;
+      
+      default:
+        applyFilter();
+        break;
     }
   }
 
   useEffect(() => {
-    setFilterProducts(products);
-  }, [])
+    applyFilter()
+  },[category,subCategory])
   
-  useEffect(() => {
-    console.log(category);
-  },[category])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -57,15 +94,15 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Topwear'} />Topwear
+              <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/>Topwear
             </p>
 
              <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Bottomwear'} />Bottomwear
+              <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/>Bottomwear
             </p>
 
              <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Winterwear'} />Winterwear
+              <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/>Winterwear
             </p>
           </div>
         </div>
