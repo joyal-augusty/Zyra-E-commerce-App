e97954a094af12bda220c6d73552a9505f817466
joyal m a <joyalaugustine12@gmail.com>
2026-03-14 23:25:59 +0530

    edited product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -33,10 +33,13 @@ useEffect(() => {
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
               productData.image.map((item, index) => (
-                <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
               ))
             }
-            </div>
+          </div>
+          <div className='w-full sm:w-[80%]'>
+              <img className='w-full h-auto' src={image} alt="" />
+          </div>
         </div>
       </div>
     </div>
