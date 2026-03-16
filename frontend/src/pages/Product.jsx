8420bcdbe07e03f9fc7433761afe90bd12297736
joyal m a <joyalaugustine12@gmail.com>
2026-03-16 23:06:03 +0530

    added product rating logic

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,7 +6,7 @@ import { assets, products } from '../assets/frontend_assets/assets';
 const Product = () => {
 
   const { productId } = useParams();
-  const { produsts } = useContext(ShopContext);
+  const { produsts ,currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false)
   const [image, setImage] = useState('');
   
@@ -51,7 +51,9 @@ useEffect(() => {
             <img src={assets.star_icon} alt="" className="w-3 5" />
             <img src={assets.star_icon} alt="" className="w-3 5" />
             <img src={assets.star_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
           </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
         </div>
       </div>
     </div>
