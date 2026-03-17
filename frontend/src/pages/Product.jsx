74f4be7c5d33e32486d80a95d81d161dc04ea262
joyal m a <joyalaugustine12@gmail.com>
2026-03-17 20:51:20 +0530

    added product size options

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -54,6 +54,15 @@ useEffect(() => {
             <p className='pl-2'>(122)</p>
           </div>
           <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex felx-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index) =>(
+                <button key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
