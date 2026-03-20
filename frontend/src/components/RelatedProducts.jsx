be71761ba7224208cef5e35e63efbdbf7de2ca28
joyal m a <joyalaugustine12@gmail.com>
2026-03-20 12:23:03 +0530

    added description & review section

---
+++ frontend/src/components/RelatedProducts.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+const RelatedProducts = ({ category, subCategory }) => {
+    
+    const {products} = useContext(ShopContext)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RelatedProducts
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -68,9 +68,28 @@ useEffect(() => {
           </div>
 
           <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                <p>100% Original product.</p>
+                <p>Cash on delivery is avilable on this product.</p>
+                <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+      {/*Description & Review Section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+        </div>
+
+        <div className='flex felx-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Elevate your everyday style with a luxuriously soft tee made from premium combed cotton, featuring a tailored fit that works alone or under a blazer and retains its shape wash after wash.</p> 
+          <p>Refresh your casual rotation with a plush hoodie crafted from premium fleece, offering a relaxed fit ideal for lounging or layering, and staying cozy wash after wash.</p>
         </div>
       </div>
 
+      {/* Display related products */}
     </div>
   ) : <div className='opacity-0'></div>
 }
