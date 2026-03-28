a1b95aba6ac0b05e5be1c01c7eb7632fa867b1d8
joyal m a <joyalaugustine12@gmail.com>
2026-03-28 09:57:45 +0530

    added show product in cart logic

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
 
 const Cart = () => {
 
@@ -21,11 +22,38 @@ const Cart = () => {
           }
         }
     }
-    
+    setCartData(cartData);
   },[cartItems])
 
   return (
-    <div>
+    <div className='border-t pt-14'>
+
+      <div className='text-2xl mb-3'>
+        <Title text={'YOUR'} text2={'CART'} />
+      </div>
+
+      <div>
+        {
+          cartData.map((item, index) => {
+            
+            const productData = products.find((products) => productData._id == item._id);
+
+            return (
+              <div key={index} className='py-4 border-b text-gray-700 gird gird-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                  </div>
+                </div>
+
+              </div>
+            )
+
+          })
+        }
+      </div>
       
     </div>
   )
