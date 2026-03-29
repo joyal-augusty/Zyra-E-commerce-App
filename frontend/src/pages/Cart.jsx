2e3dbeb458bbc41544db9ba079bafa04485c4a40
joyal m a <joyalaugustine12@gmail.com>
2026-03-29 22:25:05 +0530

    show product details in cart logic

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -45,6 +45,10 @@ const Cart = () => {
                   <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex item-center gap-5 mt-2'>
+                      <p>{currency}{productData.price}</p>
+                      <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
                   </div>
                 </div>
 
