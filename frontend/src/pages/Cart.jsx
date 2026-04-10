9625d8b94320f7e129acf77019b1f1f7bc75ca6d
joyal m a <joyalaugustine12@gmail.com>
2026-04-10 20:05:51 +0530

    added delivery information

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import CartTotal from '../components/cartTotal'
 
 const Cart = () => {
 
-  const { products, currency, cartItems } = useContext(ShopContext)
+  const { products, currency, cartItems , navigate } = useContext(ShopContext)
   
   const [cartData, setCartData] = useState([]);
 
@@ -67,7 +67,7 @@ const Cart = () => {
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
           <div className='w-full text-end'>
-            <button className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
           </div>
           </div>
       </div>
